42ec3597c2fbd5f2bcfac3478000e0d7697b8d2e
Jojo <sambaangeles553@gmail.com>
2026-04-08 21:51:53 +0800

    Am doing the consrtructor for the employee details

---
+++ employeeStatus/src/main/java/org/example/details.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class details
+{
+    private String name;
+    private String why;
+    private int status;
+
+    public details(String name, String why, int status)
+    {
+        this.name = name;
+        this.why=why;
+        this.status=status;
+    }
+}
