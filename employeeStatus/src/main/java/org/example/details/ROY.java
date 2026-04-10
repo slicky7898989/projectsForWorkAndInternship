cb2b5d28c43da44b8fd72dcc2da447af716bc2ef
Jojo <sambaangeles553@gmail.com>
2026-04-10 13:16:41 +0800

    Help

---
+++ employeeStatus/src/main/java/org/example/details/ROY.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public enum ROY
+{
+    RED,
+    ORANGE,
+    YELLOW,
+}
--- employeeStatus/src/main/java/org/example/details/details.java
+++ employeeStatus/src/main/java/org/example/details/details.java
@@ -4,9 +4,9 @@ public class details
 {
     private String name;
     private String why;
-    private int status;
+    private String status;
 
-    public details(String name, String why, int status)
+    public details(String name, String why, String status)
     {
         this.name = name;
         this.why=why;
