54ceacf67bb8d3293b8b3adf54c182c004be25fa
Jojo <sambaangeles553@gmail.com>
2026-04-16 11:27:38 +0800

    Put in DB commands. Hope to do more later

---
+++ storeBugNamesAndDescriptions/src/main/java/org/example/DBFunctions.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBFunctions
+{
+    private String use="root";
+    private String pass="rider";
+    private String link="jdbc:mysql://127.0.0.1:3306";
+    private Connection dent;
+    private Statement lent;
+
+
+    public void create()
+    {
+        String ab= "CREATE DATABASE BUG_DOCUMENTATION ";
+
+        {
+            try
+            {
+                dent = DriverManager.getConnection(link,use,pass);
+                lent =  dent.createStatement();
+                lent.executeUpdate(ab);
+                System.out.println("Hi");
+            }
+            catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public void table()
+    {
+        link="jdbc:mysql://127.0.0.1:3306/BUG_DOCUMENTATION";
+        String ba="CREATE TABLE documentation(Bug_Name Varchar(1000) Not Null, Bug_Description Varchar(1000) Not Null)";
+        try
+        {
+            dent = DriverManager.getConnection(link,use,pass);
+            lent =  dent.createStatement();
+            lent.executeUpdate(ba);
+            System.out.println("HI!");
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void insertToTable()
+    {
+        link="jdbc:mysql://127.0.0.1:3306/BUG_DOCUMENTATION";
+        String ba="INSERT INTO documentation VALUES ('Kento', 'It exists');";
+        try
+        {
+            dent = DriverManager.getConnection(link,use,pass);
+            lent =  dent.createStatement();
+            lent.executeUpdate(ba);
+            System.out.println("HI!");
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ storeBugNamesAndDescriptions/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        DBFunctions func = new DBFunctions();
+        func.insertToTable();
+    }
+}
