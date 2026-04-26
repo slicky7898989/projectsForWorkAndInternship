748a27cc06a79bda1d52ef6fc563548880941e53
Jojo <sambaangeles553@gmail.com>
2026-04-26 18:44:36 +0800

    A

--- totalNumberOfBugsAndErrors/src/main/java/org/example/choice.java
+++ totalNumberOfBugsAndErrors/src/main/java/org/example/choice.java
@@ -59,49 +59,77 @@ public class choice
         //ounter.goThrough();
 
 
-        System.out.println("Type in the input: ");
+
         int inp;
         String azz = "";
+        String mn = "";
 
         do
         {
-            inp= tn.nextInt();
+            System.out.println("1. Insert data to table");
+            System.out.println("2. Output data from table");
+            System.out.println("3. Update table content");
+            System.out.println("4. Delete data from table");
+            System.out.println("5. Tally up all the bugs");
+            System.out.println("6. Exit");
+            System.out.println("Type in the input: ");
+            mn = "";
+            inp=0;
+            do
+            {
+                try
+                {
+                    azz="";
+                    inp= tn.nextInt();
+                    tn.nextLine();
+                    if(inp != 1 && inp != 2 && inp != 3 && inp != 4 && inp != 5  && inp != 6)
+                    {
+                        azz= "F";
+                        System.out.println("Only from 1-6");
+                    }
+                }catch (Exception e)
+                {
+                    tn.nextLine();
+                    azz="F";
+                    System.out.println("Only Numbers");
+                }
+            }while (azz.equals("F"));
+
             switch (inp)
             {
                 case 1:
                     ate.insertToTable();
+                    azz = "F";
                     break;
 
                 case 2:
                     ad.readTable();
+                    azz = "F";
                     break;
 
                 case 3:
                     date.update();
+                    azz = "F";
                     break;
 
                 case 4:
                     elete.del();
+                    azz = "F";
                     break;
 
                 case 5:
                     ounter.goThrough();
+                    azz = "F";
                     break;
 
                 case 6:
-                    System.out.println("Continue? y/n");
-                    do
-                    {
-                        azz=tn.nextLine();
+                    mn = "O";
 
-                    }while (!azz.equals("y") || !azz.equals("n") );
 
-                default:
-                    azz="F";
 
             }
 
-        }while (azz.equals("F") || azz.equals("y"));//Hi
+        }while (azz.equals("F") || mn.equals("S"));//Hi
 
 
     }
