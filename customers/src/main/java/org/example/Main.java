368a75073bb26d87c3ff6a81169e554517f3f32d
Jojo <sambaangeles553@gmail.com>
2026-05-16 07:44:59 +0800

    Added Main

---
+++ customers/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+import org.example.operations.*;
+public class Main extends add
+{
+    public static void main(String[] args)
+    {
+        Main jen = new Main();
+        for(int z =0;z<2;z++)
+        {
+            jen.add1();
+        }
+        jen.ed();
+        jen.ow();
+    }
+}
+
+/**
+ * 1. Add Customer
+ * 2. Show All Customers
+ * 3. Edit Customer
+ * 4. Delete Customer
+ * 5. The average age of the customers
+ * Inputs:
+ * ID
+ * Name
+ * Age
+ * Mood
+ * Prioritaztion
+ * **/
